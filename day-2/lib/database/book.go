483f93e5b76e0ba884413ec285e9dcfbb2c5728f
ilmanurh <ilma.hidayati@medicplus.co.id>
2022-09-18 15:26:06 +0700

    Change to static API

--- day-2/lib/database/book.go
+++ day-2/lib/database/book.go
@@ -1,52 +1,53 @@
 package database
 
 import (
-	"github.com/nurhidaylma/alterra-agmc/day-2/config"
+	"time"
+
 	"github.com/nurhidaylma/alterra-agmc/day-2/models"
 )
 
 func GetBook() (interface{}, error) {
-	var books []models.Book
+	now := time.Now().UTC()
 
-	if err := config.DB.Find(&books).Error; err != nil {
-		return nil, err
-	}
-
-	return books, nil
-}
-
-func GetBookByID(bookID uint) (*models.Book, error) {
-	book := models.Book{}
-	if err := config.DB.Where("id = ?", bookID).First(&book).Error; err != nil {
-		return nil, err
-	}
-
-	return &book, nil
-}
-
-func CreateBook(book *models.Book) (interface{}, error) {
-	if err := config.DB.Create(book).Error; err != nil {
-		return nil, err
+	book := &models.Book{
+		ID:        1,
+		Title:     "The Little House",
+		Writer:    "Laura Wilder",
+		NoOfPage:  150,
+		Stock:     12,
+		Price:     1500,
+		CreatedAt: now,
+		UpdatedAt: now,
 	}
 
 	return book, nil
 }
 
-func UpdateBook(book *models.Book) (interface{}, error) {
-	if err := config.DB.Model(book).Updates(book).Error; err != nil {
-		return nil, err
-	}
-	if err := config.DB.First(&book, "id = ?", book.ID).Error; err != nil {
-		return nil, err
+func GetBookByID(bookID uint) (*models.Book, error) {
+	now := time.Now().UTC()
+
+	book := &models.Book{
+		ID:        1,
+		Title:     "The Little House",
+		Writer:    "Laura Wilder",
+		NoOfPage:  150,
+		Stock:     12,
+		Price:     1500,
+		CreatedAt: now,
+		UpdatedAt: now,
 	}
 
+	return book, nil
+}
+
+func CreateBook(book *models.Book) (interface{}, error) {
+	return book, nil
+}
+
+func UpdateBook(book *models.Book) (interface{}, error) {
 	return &book, nil
 }
 
 func DeleteBook(book *models.Book) error {
-	if err := config.DB.Delete(&book).Error; err != nil {
-		return err
-	}
-
 	return nil
 }
